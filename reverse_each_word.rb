0250902f099a4c39adc2e1131d132c1948db4ea7
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-17 02:42:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,8 @@ def reverse_each_word(sent)
   sent_array = sentence.split(" ")
   rev_array = []
   
+  rev_array << sent.reverse
+  
   rev_sentence = sent_array.each { |word| word.reverse }
   rev_sentence
   # rev_sentence.each  
