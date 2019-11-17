27879a3f3c8010ab897d5cc98f9c05f6f518e22d
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-17 02:41:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,24 +1,6 @@
-# def reverse_each_word(str)
-#   rev_array = []
-#   rev_array << str.split(" ")
-#   p rev_array
-  
-#   rev_sentence = ""
-#   i = 0 
-#   while i < rev_array.length 
-#   rev_el = rev_array[i].reverse
-#   rev_sentence = rev_array[i]
-#   i += 1
-#   end 
-  
-#   return rev_sentence
-# end 
-
 def reverse_each_word(sent)
-  # split_sentence = []
-  # split_sentence << sentence.split(" ")
-  
   sent_array = sentence.split(" ")
+  
   rev_sentence = sent_array.each { |word| word.reverse }
   rev_sentence
   # rev_sentence.each  
